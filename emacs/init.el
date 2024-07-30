25619866f191a735701a9b7cfa50b928264242ad
iskyd <iskyd@proton.me>
2024-07-30 22:33:27 +0200

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,23 +1,18 @@
 (require 'package)
+(require 'image)
+
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'use-package)
 
-
-(defun kill-other-buffers ()
-    "Kill all other buffers."
-    (interactive)
-    (mapc 'kill-buffer 
-          (delq (current-buffer) 
-                (remove-if-not 'buffer-file-name (buffer-list)))))
-
 ;; Reload current buffer
 (defun revert-buffer-no-confirm ()
-    "Revert buffer without confirmation."
-    (interactive)
-    (revert-buffer :ignore-auto :noconfirm))
+  "Revert buffer without confirmation."
+  (interactive)
+  (revert-buffer :ignore-auto :noconfirm))
 
+;; kill all buffers
 (defun kill-all-buffers ()
   "Kill all buffers, leaving only the *scratch* buffer."
   (interactive)
@@ -28,22 +23,35 @@
 (use-package use-package-ensure-system-package
   :ensure t)
 
+(use-package exec-path-from-shell
+  :ensure t)
+(when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
 (use-package projectile
   :ensure t
   :init
   (projectile-mode +1)
   (setq projectile-project-search-path '("~/dev/" "/opt/projects/Conio/" "/opt/projects/Conio/clients"))
   (projectile-register-project-type 'zig '("build.zig")
-                                  :project-file "build.zig"
-                                  :compile "zig build"
-                                  :test "zig build test"
-				  :run "zig build run"
-                                  )
+                                    :project-file "build.zig"
+                                    :compile "zig build"
+                                    :test "zig build test"
+                                    :run "zig build run"
+                                    )
   :bind (:map projectile-mode-map
               ("s-p" . projectile-command-map)
               ("C-c p" . projectile-command-map))
   :hook
   (after-init . projectile-global-mode))
+
+(use-package magit
+  :ensure t
+  )
 
 (use-package lsp-mode
   :ensure t
@@ -52,27 +60,34 @@
   (setq lsp-keymap-prefix "C-c l")
   (setq lsp-enable-file-watchers nil)
   (setq lsp-file-watch-threshold 2500)
+  (setq lsp-completion-enable t)
+  (setq lsp-enable-snippet nil)
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (zig-mode . lsp-deferred)
-	 (python-mode . lsp-deferred)
-	 (c-mode . lsp-deferred)
-	 (go-mode . lsp-deferred)
-	 (haskell-mode . lsp-deferred)
+         (python-mode . lsp-deferred)
+         (c-mode . lsp-deferred)
+         (go-mode . lsp-deferred)
          ;; if you want which-key integration
          ;; (lsp-mode . lsp-enable-which-key-integration)
-	 )
+         )
   :commands lsp-deferred)
 
 (use-package lsp-treemacs
   :ensure t)
 
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  )
+
 (use-package zig-mode
   :ensure t
   :init
   (add-hook 'zig-mode-hook
-          (lambda ()
-            (setq compile-command "zig build"))
-  )
+            (lambda ()
+              (setq compile-command "zig build"))
+            )
   )
 
 (use-package flycheck
@@ -86,17 +101,17 @@
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
-			 (require 'lsp-pyright)
-			 (setq lsp-pyright-venv-path "$VENV_DIR")
-			 (lsp-deferred)))
+                         (require 'lsp-pyright)
+                         (setq lsp-pyright-venv-path "$VENV_DIR")
+                         (lsp-deferred)))
   )
 
 (use-package yaml-mode
   :ensure t
   :init
   (add-hook 'yaml-mode-hook
-          (lambda ()
-            (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+            (lambda ()
+              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
   )
 
 (use-package json-mode
@@ -104,10 +119,6 @@
   )
 
 (use-package go-mode
-  :ensure t
-  )
-
-(use-package haskell-mode
   :ensure t
   )
 
@@ -122,11 +133,31 @@
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown"))
 
+(use-package mermaid-mode
+  :ensure t
+  )
+
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 1)
+  (setq doom-modeline-project-detection 'projectile)
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-major-mode-icon t)
+  (setq doom-modeline-major-mode-color-icon t)
+  (setq doom-modeline-env-version t)
+  )
+
+(use-package nerd-icons
+  :ensure t
+  )
+
 ;; Emacs config
 (setq inhibit-startup-screen t)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
-(setq display-line-numbers-type 'relative) 
+(scroll-bar-mode 0)
+(setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 
 (custom-set-variables
@@ -143,7 +174,7 @@
  ;; If there is more than one, they won't work right.
  )
 
-(set-face-attribute 'default nil :height 200)
+(set-face-attribute 'default nil :height 100)
 (add-to-list 'default-frame-alist '(alpha 97)) ;; doesnt work on emacs29 and X
 (setq custom-file (make-temp-file "emacs-custom"))
 (setq backup-directory-alist '(("." . "~/emacsbackup")))
@@ -160,6 +191,7 @@
 (global-set-key (kbd "C-c c") 'compile)
 
 ;; Auto Mode Alist
+
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
 ;; Use a on dired
